2be78413e78308e9642811ba42426b23b5ad6d83
mhel.farthing <huqq1917@outlook.com>
2021-01-20 20:54:25 +0800

    change module name to file name

--- Verilog/source/SPI_Slave.v
+++ Verilog/source/SPI_Slave.v
@@ -21,7 +21,7 @@
 //              More info: https://en.wikipedia.org/wiki/Serial_Peripheral_Interface_Bus#Mode_numbers
 ///////////////////////////////////////////////////////////////////////////////
 
-module SPISLAVE
+module SPI_Slave
   #(parameter SPI_MODE = 0)
   (
    // Control/Data Signals,
